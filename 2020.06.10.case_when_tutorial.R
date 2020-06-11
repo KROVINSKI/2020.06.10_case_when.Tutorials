65e16c55bdba2e1c41fa6452c0d8cc5921926793
Katherine Rovinski <katherinerovinski@Katherines-MacBook-Pro.local>
2020-06-11 14:32:18 -0700

    2020.06.11 1430 PST why does it still not work?!

--- 2020.06.10.case_when_tutorial.R
+++ 2020.06.10.case_when_tutorial.R
@@ -1,3 +1,86 @@
 # Hello World 
 
 # Below is a case when tutorial 
+
+
+#********************************
+# Libraries
+#********************************
+library(dplyr)
+
+# _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _|
+
+
+#********************************
+# Table of Contents
+#********************************
+
+# 1.0 Introduction
+# 2.0 Bringing in Data
+# 3.0 Creating a new expression
+
+# _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _|
+
+#********************************
+# 1.0 Introduction
+#********************************
+
+# Case when in R can be executed with case_when() 
+#   function in dplyr package. Dplyr package is provided with case_when() 
+#   function which is similar to case when statement in SQL.
+
+# We will be using iris data to depict the example of case_when() 
+#   function.
+
+# _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _|
+
+
+#********************************
+# 2.0 My data 
+#********************************
+
+mydata2 <-iris 
+head(mydata2)
+
+
+
+# _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _|
+
+
+#********************************
+# 3.0 New Conditional Statement 
+#********************************
+
+# Create new/additional variable 
+# species_new using the mutate function and case when statement
+
+mydata2 %>% mutate(species_new = case_when(is.na(Species) ~ "missing",
+                                           Species=="setosa" ~ "setosa_new",
+                                           Species=="versicolor" ~ "versicolor_new",
+                                           Species=="virginica" ~ "virginica_new",
+                                           TRUE ~ "others"))
+# you can use variables directly within the case_when()wrapper
+# TRUE equivalent to ELSE statement
+
+
+mydata2
+head(mydata2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+#********************************
+# End of Document | End of Script
+#********************************
+
+
+
