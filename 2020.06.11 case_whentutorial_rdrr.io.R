b4ca9413ea91478f9131853c15358669a5aa3b9b
Katherine Rovinski <katherinerovinski@Katherines-MacBook-Pro.local>
2020-06-11 14:35:43 -0700

    2020.06.11 1433PST creating the new vector

---
+++ 2020.06.11 case_whentutorial_rdrr.io.R
@@ -0,0 +1,22 @@
+# Hello World 
+# Title     case_when: A general vectorised if
+# Author    https://rdrr.io/cran
+# Student   Kate Rovinski
+# Date      2020.06.11
+
+# Below is a case when tutorial 
+# Original link https://rdrr.io/cran/dplyr/man/case_when.html
+
+#********************************
+# Libraries
+#********************************
+library(dplyr)
+
+# _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _|
+
+
+#********************************
+# Table of Contents
+#********************************
+
+# 1.0 Introduction
